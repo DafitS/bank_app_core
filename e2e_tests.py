c05228d62e6da661e57267c377e817efe0c0182b
DafitS <dw220105@mail.com>
2026-01-19 07:34:54 +0100

    Wrote tests

--- e2e_tests.py
+++ e2e_tests.py
@@ -1,43 +1,71 @@
 import requests
-import pytest
+import pytest, os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from orm import Base
 
-engine = create_engine('postgresql://postgres:1234@localhost:5432/bank_app2')
+# Poprawny connection string do Twojego Dockera
+value = os.getenv("URL")
+engine = create_engine(value)
 Session = sessionmaker(bind=engine)
 
 @pytest.fixture(scope="session", autouse=True)
 def setup_database():
-
+    
     Base.metadata.drop_all(bind=engine)
     Base.metadata.create_all(bind=engine)
 
     yield
 
-
+    
     Base.metadata.drop_all(bind=engine)
+
 
 def test_create_user():
     payload = {"email": "test@local.cm"}
-    response = requests.post("http://127.0.0.1:8000/user", json = payload)
+    response = requests.post("http://127.0.0.1:8000/user", json=payload)
     assert response.status_code in [200, 201, 204]
 
 
 def test_list_users():
     payload = {"email": "test22@local.cm"}
-    requests.post("http://127.0.0.1:8000/user", json = payload)
-    response = requests.get("http://127.0.0.1:8000/users")
-    users = response.json()
-    assert any(u[1] == "test22@local.cm" for u in users)
+    requests.post("http://127.0.0.1:8000/user", json=payload)
 
+    response = requests.get("http://127.0.0.1:8000/users")
+    assert response.status_code == 200
+
+    users = response.json()
+    assert any(u["email"] == "test22@local.cm" for u in users)
 
 
 def test_create_account():
     payload = {"email": "test33@local.cm"}
-    requests.post("http://127.0.0.1:8000/user", json = payload)
-    user = requests.get("http://127.0.0.1:8000/users").json()
-    user_id = user[-1][0]
+    requests.post("http://127.0.0.1:8000/user", json=payload)
+
+    users = requests.get("http://127.0.0.1:8000/users").json()
+    user_id = users[-1]["id"]
+
     payload = {"user_id": user_id}
-    response = requests.post("http://127.0.0.1:8000/account", json = payload)
-    assert response.status_code in [200, 204, 201]
+    response = requests.post("http://127.0.0.1:8000/account", json=payload)
+
+    assert response.status_code in [200, 201, 204]
+
+
+def test_transaction():
+    
+    requests.post("http://127.0.0.1:8000/user", json={"email": "test43@local.cm"})
+    requests.post("http://127.0.0.1:8000/user", json={"email": "test53@local.cm"})
+
+    users = requests.get("http://127.0.0.1:8000/users").json()
+
+    acc_from = users[-1]["account_id"]
+    acc_to = users[-2]["account_id"]
+
+    transaction_payload = {
+        "account_from_id": acc_from,
+        "account_to_id": acc_to,
+        "amount": 250
+    }
+
+    response = requests.post("http://127.0.0.1:8000/transaction", json=transaction_payload)
+    assert response.status_code in [200, 201, 204]
