85772d163f8de7173fdd1027b2ef99ca68d262cd
DafitS <dw220105@mail.com>
2026-01-29 20:07:34 +0100

    Fix e2eTest and setup powershell template for db

--- e2e_tests.py
+++ e2e_tests.py
@@ -4,31 +4,30 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from orm import Base
 
-# Poprawny connection string do Twojego Dockera
 value = os.getenv("URL")
 engine = create_engine(value)
 Session = sessionmaker(bind=engine)
 
 @pytest.fixture(scope="session", autouse=True)
 def setup_database():
-    
     Base.metadata.drop_all(bind=engine)
     Base.metadata.create_all(bind=engine)
-
     yield
-
-    
     Base.metadata.drop_all(bind=engine)
 
-
 def test_create_user():
-    payload = {"email": "test@local.cm"}
+    payload = {
+        "email": "test@local.cm",
+        "password": "Aa123456!"
+    }
     response = requests.post("http://127.0.0.1:8000/user", json=payload)
     assert response.status_code in [200, 201, 204]
 
-
 def test_list_users():
-    payload = {"email": "test22@local.cm"}
+    payload = {
+        "email": "test22@local.cm",
+        "password": "Aa123456!"
+    }
     requests.post("http://127.0.0.1:8000/user", json=payload)
 
     response = requests.get("http://127.0.0.1:8000/users")
@@ -37,9 +36,11 @@ def test_list_users():
     users = response.json()
     assert any(u["email"] == "test22@local.cm" for u in users)
 
-
 def test_create_account():
-    payload = {"email": "test33@local.cm"}
+    payload = {
+        "email": "test33@local.cm",
+        "password": "Aa123456!"
+    }
     requests.post("http://127.0.0.1:8000/user", json=payload)
 
     users = requests.get("http://127.0.0.1:8000/users").json()
@@ -47,6 +48,4 @@ def test_create_account():
 
     payload = {"user_id": user_id}
     response = requests.post("http://127.0.0.1:8000/account", json=payload)
-
     assert response.status_code in [200, 201, 204]
-
---
+++ set_db.ps1
@@ -0,0 +1,6 @@
+$Env:URL="postgresql://admin:1234@postgres18:5432/bank_app_core"
+$Env:USER="admin"
+$Env:DB="bank_app_core"
+$Env:PASSWORD="1234"
+
+Write-Host "Zmienne Srodowiskowe Ustawione!"
